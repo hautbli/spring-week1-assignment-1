d307ace843576f78447b707e9386f44cd01c252e
hautblis <hautblis@gmail.com>
2022-09-20 19:50:06 +0900

    코드리뷰 : 예외를 던지는 메서드에 JavaDoc 추가

--- app/src/main/java/com/codesoom/assignment/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/TaskService.java
@@ -14,6 +14,11 @@ public class TaskService {
         return JsonParser.objectToJson(tasks);
     }
 
+    /**
+     * @param id 조회할 Task의 id이다.
+     * @return 조회한 Task를 Json으로 변환한 값이다.
+     * @throws TaskNotFoundException 조회할 Task가 존재하지 않는 경우이다.
+     */
     public String getTask(Long id) throws IOException {
         if (taskRepository.isExist(id)) {
             Task task = taskRepository.findById(id);
@@ -29,6 +34,12 @@ public class TaskService {
         return JsonParser.objectToJson(savedTask);
     }
 
+    /**
+     * @param id   수정할 Task의 id이다.
+     * @param body Task정보로 변환하려는 body이다.
+     * @return 수정한 Task를 Json으로 변환한 값이다.
+     * @throws TaskNotFoundException 조회할 Task가 존재하지 않는 경우이다.
+     */
     public String updateTask(Long id, String body) throws IOException {
         if (taskRepository.isExist(id)) {
             Task task = JsonParser.requestBodyToObject(body, Task.class);
@@ -41,6 +52,10 @@ public class TaskService {
         throw new TaskNotFoundException();
     }
 
+    /**
+     * @param id 조회할 Task의 id이다.
+     * @throws TaskNotFoundException 조회할 Task가 존재하지 않는 경우이다.
+     */
     public void deleteTask(Long id) {
         if (taskRepository.isExist(id)) {
             taskRepository.delete(id);
--- app/src/main/java/com/codesoom/http/HttpMethod.java
+++ app/src/main/java/com/codesoom/http/HttpMethod.java
@@ -21,6 +21,13 @@ public enum HttpMethod {
         return this == DELETE;
     }
 
+    /**
+     *
+     * @param method HttpMethod 이름에 해당하는 값이다.
+     * @return 주어진 문자열에 해당하는 HttpMethod를 반환한다.
+     * @throws MethodNotExistException
+     *         HttpMethod에 없는 value가 온 경우이다.
+     */
     public static HttpMethod of(String method) {
         try {
             return HttpMethod.valueOf(method);
